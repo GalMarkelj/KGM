70279dfec5862fd7c37cf1e8e3aa4902f1dcfd1b
GalMarkelj <galmarkelj09@gmail.com>
2022-06-21 16:33:13 +0200

    Set site title

---
+++ src/components/SiteTitle.jsx
@@ -0,0 +1,9 @@
+const SiteTitle = () => (
+  <div className="container m-top--xxl p-side--s">
+    <p class="f--s m--reset">Hi, my name is <span class="d--block h1 f--bold f--secondary">Klavdij - Gal Markelj</span></p>
+    <p class="m-top--s f--l m--reset">I'm learning to become a web developer and</p>
+    <p class="f--l m--reset">with this website you can see who I am and my progress.</p>
+  </div>
+)
+
+export default SiteTitle
