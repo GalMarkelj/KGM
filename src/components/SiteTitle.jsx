e426780d8be5d7e495da4b63aad1930904da77d9
GalMarkelj <galmarkelj09@gmail.com>
2022-06-22 08:39:57 +0200

    Change layout position of the title

--- src/components/SiteTitle.jsx
+++ src/components/SiteTitle.jsx
@@ -1,5 +1,5 @@
 const SiteTitle = () => (
-  <div className="container m-top--xxl p-side--s">
+  <div className="container m-top--xxl p-side--s site-title">
     <p class="f--s m--reset">Hi, my name is <span class="d--block h1 f--bold f--secondary">Klavdij - Gal Markelj</span></p>
     <p class="m-top--s f--l m--reset">I'm learning to become a web developer and</p>
     <p class="f--l m--reset">with this website you can see who I am and my progress.</p>
