9851c67253da00f38d0fafbc2c611d5e563e602e
GalMarkelj <galmarkelj09@gmail.com>
2022-06-22 09:15:53 +0200

    Set first look to the page to be full height

--- src/components/IndexPage.jsx
+++ src/components/IndexPage.jsx
@@ -5,9 +5,11 @@ import mainBackground from '../images/background/main-background.jpg'
 
 const IndexPage = () => (
   <div>
-    <Header />
-    <img src={mainBackground} alt="Main background" className="main-background fixed--top width--100p height--100p" />
-    <SiteTitle />
+    <div className="full--height">
+      <Header />
+      <img src={mainBackground} alt="Main background" className="main-background fixed--top width--100p height--100p" />
+      <SiteTitle />
+    </div>
   </div>
 )
 
