73f5fed39629d862a94a493c8399a8cafce38875
GalMarkelj <galmarkelj09@gmail.com>
2022-06-23 16:36:41 +0200

    Create makeClassNameDecorator utility

---
+++ src/helpers/utils.js
@@ -0,0 +1,8 @@
+const makeClassNameDecorator = (classPrefix) => {
+  return (mods = [], extraClass = '') => 
+    mods.reduce((acc, curr) => ( `${acc} ${classPrefix}--${curr}`,
+    `${extraClass} ${classPrefix}`.trim())
+  )
+}
+
+export default { makeClassNameDecorator }
