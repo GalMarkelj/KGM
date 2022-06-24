09000be483eaf406ac73e9be98ac2d1c5f0f07a2
GalMarkelj <galmarkelj09@gmail.com>
2022-06-24 10:20:53 +0200

    Rename CLickElement component to Buttons, restructure it and use them

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,10 @@
-import ClickElement from "./ClickElement"
+import { Button } from "./Button"
 
 const Banner = ({title, paragraph, clickElement}) => (
   <div className="banner t--center">
     <h3 className="h1 f--secondary">{title}</h3>
     <p className="f--l">{paragraph}</p>
-    <ClickElement {...clickElement}/>
+    <Button mods={['barbie-dark']} className="f--m">Email me</Button>
   </div>
 )
 
