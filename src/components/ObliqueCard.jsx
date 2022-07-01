10b006f1935908ff7dba4773e11dbc57d5179087
GalMarkelj <galmarkelj09@gmail.com>
2022-07-01 08:59:02 +0200

    Change layout and date type and displayment

--- src/components/ObliqueCard.jsx
+++ src/components/ObliqueCard.jsx
@@ -7,25 +7,31 @@ const ObliqueCard = ({
   listItems,
   image,
   cardExternal,
-  alignLeft }) => (
-  <div
-    className={`oblique-card p-side--s pos--rel container m-top--xxl 
-    ${alignLeft ? 'oblique-card--reverse' : ' '}`}
-    data-card-title={title}
-  >
-    <div className='oblique-card__content'>
-      <span className='f--s'>{date}</span>
-      <h3 className='c--sky m--reset f--secondary h2'>{title}</h3>
-      {!!inProgress && <span className='f--xs'>~ In progress ~</span>}
-      <p className='oblique-card__content-description f--l m-top--s'>{description}</p>
-      <h4 className='m--reset'>{list}</h4>
-      <ul className='list--inline flex flex--end p--reset m-top--xs'>
-        {listItems.map((listItem) => <li>{listItem}</li>)}
-      </ul>
-      {!!cardExternal && <span>For more info click <a href={cardExternal} target='_blank' rel='noreferrer'>Here</a></span>}
-    </div>
-    <img src={image} alt='Card image' className='oblique-card__image p--m' />
-  </div>
-)
+  alignLeft }) => {
+    console.log(date)
+    const day = date.getDate()
+    const month = date.toLocaleString('default', { month: 'long' })
+    const year = date.getFullYear()
+    return (
+      <div
+        className={`oblique-card p-side--s pos--rel container m-top--xxl 
+        ${alignLeft ? 'oblique-card--reverse' : ' '}`}
+        data-card-title={title}
+      >
+        <div className='oblique-card__content'>
+          <span className='f--s'>{`${day}. ${month}, ${year}`}</span>
+          <h3 className='c--sky m--reset f--secondary h2'>{title}</h3>
+          {!!inProgress && <span className='f--xs'>~ In progress ~</span>}
+          <p className='oblique-card__content-description f--l m-top--s'>{description}</p>
+          <h4 className='m--reset'>{list}</h4>
+          <ul className='list--inline flex flex--end p--reset m-top--xs'>
+            {listItems.map((listItem) => <li className="oblique-card__list-item">{listItem}</li>)}
+          </ul>
+          {!!cardExternal && <span>For more info click <a href={cardExternal} target='_blank' rel='noreferrer'>Here</a></span>}
+        </div>
+        <img src={image} alt='Card image' className='oblique-card__image p--m' />
+      </div>
+    )
+  }
 
 export default ObliqueCard
